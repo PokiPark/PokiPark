f94d32a636019e7db9b233fef4bd5dc2bd17ebbf
TobRojekt <tobrojekt2@gmail.com>
2021-06-21 12:50:06 +0200

    changed primaryStage title to "PokiPark | Login"

--- PokiPark/src/application/Main.java
+++ PokiPark/src/application/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("Login.fxml"));
 			Scene scene = new Scene(root, 600, 500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setTitle("Login");
+			primaryStage.setTitle("PokiPark | Login");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (Exception e) {
