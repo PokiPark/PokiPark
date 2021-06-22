37401c0352ed162415333f63493415f72a3322fb
TobRojekt <tobrojekt2@gmail.com>
2021-06-22 13:02:40 +0200

    added usernameLabel initialization

--- PokiPark/src/application/AdminMainMenuController.java
+++ PokiPark/src/application/AdminMainMenuController.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.io.*;
+import java.net.*;
+import java.util.*;
 
 import javafx.event.*;
 import javafx.fxml.*;
@@ -8,31 +10,30 @@ import javafx.scene.control.*;
 import javafx.scene.image.*;
 import javafx.scene.input.*;
 
-public class AdminMainMenuController extends Main {
+public class AdminMainMenuController extends Main implements Initializable {
 
-	@FXML
-	private ImageView avatar;
-	@FXML
-	private Label usernameLabel;
+	@FXML private ImageView avatar;
+	
+	@FXML private Label usernameLabel;
 
-	@FXML
-	public void avatarClicked(MouseEvent event) {
+	@FXML public void avatarClicked(MouseEvent event) {
 		System.out.println("Test");
 		// Menu klappt sich aus mit >Einstellungen und >Statistik und so
 	}
 
-	@FXML
-	public void pokedexClicked(ActionEvent event) throws IOException {
+	@FXML public void pokedexClicked(ActionEvent event) throws IOException {
 		changeStageTo(event, "AdminPokedex");
 	}
 
-	@FXML
-	public void zonesClicked(ActionEvent event) throws IOException {
+	@FXML public void zonesClicked(ActionEvent event) throws IOException {
 		changeStageTo(event, "AdminZones");
 	}
 
-	@FXML
-	public void logoutClicked(ActionEvent event) throws IOException {
+	@FXML public void logoutClicked(ActionEvent event) throws IOException {
 		changeStageTo(event, "Login");
+	}
+	
+	@Override public void initialize(URL url, ResourceBundle rb) {
+		usernameLabel.setText(Database.activeUser.getUsername() + " (Admin)");
 	}
 }
--- PokiPark/src/application/MainMenuController.java
+++ PokiPark/src/application/MainMenuController.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.io.*;
+import java.net.*;
+import java.util.*;
 
 import javafx.event.*;
 import javafx.fxml.*;
@@ -8,31 +10,28 @@ import javafx.scene.control.*;
 import javafx.scene.image.*;
 import javafx.scene.input.*;
 
-public class MainMenuController extends Main {
+public class MainMenuController extends Main implements Initializable {
 
-	@FXML
-	private ImageView avatar;
-	@FXML
-	private Label usernameLabel;
+	@FXML private ImageView avatar;
+	@FXML private Label usernameLabel;
 
-	@FXML
-	public void avatarClicked(MouseEvent event) {
-		System.out.println("Test");
+	@FXML public void avatarClicked(MouseEvent event) {
 		// Menu klappt sich aus mit >Einstellungen und >Statistik und so
 	}
 
-	@FXML
-	public void pokedexClicked(ActionEvent event) throws IOException {
+	@FXML public void pokedexClicked(ActionEvent event) throws IOException {
 		changeStageTo(event, "Pokedex");
 	}
 
-	@FXML
-	public void zonesClicked(ActionEvent event) throws IOException {
+	@FXML public void zonesClicked(ActionEvent event) throws IOException {
 		changeStageTo(event, "Zones");
 	}
 
-	@FXML
-	public void logoutClicked(ActionEvent event) throws IOException {
+	@FXML public void logoutClicked(ActionEvent event) throws IOException {
 		changeStageTo(event, "Login");
+	}
+	
+	@Override public void initialize(URL url, ResourceBundle rb) {
+		usernameLabel.setText(Database.activeUser.getUsername());
 	}
 }
